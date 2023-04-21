7d7552ed29af6d1d35d7eabb7397ba56adc72c4c
sergioelenes <87728823+sergioelenes@users.noreply.github.com>
2023-04-21 12:20:27 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_apscheduler import APScheduler
+from datetime import datetime, timedelta
+from apscheduler.triggers.interval import IntervalTrigger
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bt = os.getenv('bt')
+cid = os.getenv('cid')
+
+app = Flask(__name__)
+scheduler = APScheduler()
+
+# Configuración de la programación en segundo plano
+def send_telegram_message():
+    now=datetime.now()
+    bot_token = bt
+    chat_id = cid
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    message = f'Hola, esto es una prueba de mensaje en Telegram | {now}'
+    data = {'chat_id': chat_id, 'text': message}
+    response = requests.post(url, data=data)
+    print(response.json())
+
+#scheduler.add_job(id='send_telegram_message', func=send_telegram_message, trigger='interval', days=3, start_date=datetime.now()+timedelta(seconds=1), timezone='America/Mazatlan')
+scheduler.add_job(id='send_telegram_message', func=send_telegram_message, trigger=IntervalTrigger(minutes=20), start_date=datetime.now()+timedelta(seconds=1))
+# Inicialización de la programación en segundo plano
+scheduler.init_app(app)
+scheduler.start()
+
+# Rutas de la aplicación Flask
+@app.route("/", methods=['GET','POST'])
+def inicio():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
